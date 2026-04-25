17e2e37408bb5a9d93d0e412a596b1fb68f5333b
aaaaaaaaaaaaaaaaaaaaer <StarcreatetorV2@proton.me>
2026-04-25 17:46:46 +0700

    Update and rename Loadstring.lua to Loadstring

--- Loadstring.lua
+++
@@ -1,8 +0,0 @@
-print("XeNOX Library has officially loaded!")
-
-local p = Instance.new("Part")
-p.Parent = game.Workspace
-p.Position = Vector3.new(0, 50, 0)
-p.Size = Vector3.new(10, 10, 10)
-p.BrickColor = BrickColor.new("Bright blue")
-p.Anchored = true
