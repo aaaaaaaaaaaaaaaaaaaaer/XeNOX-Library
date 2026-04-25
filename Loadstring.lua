5ecd8a918fac3947f733cb98238b2c0d2d962214
aaaaaaaaaaaaaaaaaaaaer <StarcreatetorV2@proton.me>
2026-04-25 17:45:46 +0700

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,8 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/aaaaaaaaaaaaaaaaaaaaer/XeNOX-Library/Loadstring.luau"))()
+print("XeNOX Library has officially loaded!")
+
+local p = Instance.new("Part")
+p.Parent = game.Workspace
+p.Position = Vector3.new(0, 50, 0)
+p.Size = Vector3.new(10, 10, 10)
+p.BrickColor = BrickColor.new("Bright blue")
+p.Anchored = true
