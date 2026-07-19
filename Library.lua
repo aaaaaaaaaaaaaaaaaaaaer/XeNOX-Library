f52a526cc19b494b0e6dc895744fe8e3fc80f3db
aaaaaaaaaaaaaaaaaaaaer <StarcreatetorV2@proton.me>
2026-07-19 16:59:43 +0700

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -145,6 +145,8 @@ function XELIB:MakeWindow(config)
     local menuKey = Enum.KeyCode.RightControl
     local menuOpen = true
     local isMinimized = false
+    local originalPos = mainFrame.Position
+    local originalSize = mainFrame.Size
     local tabs = {}
     local tabCount = 0
     local activeNotifs = {}
